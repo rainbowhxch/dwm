ca4efdd409851ef0a2d0d706d8926364a2816c05
rainbowhxch <rainbowhxch@gmail.com>
2021-12-25 16:03:09 +0800

    removed autostart and mv it to xprofile

--- config.def.h
+++ config.def.h
@@ -17,6 +17,7 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const Bool viewontag         = True;     /* Switch view on tag switch */
 static const char *fonts[]          = { "monospace:size=10" };
+static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
@@ -67,6 +68,7 @@ static const Layout layouts[] = {
 #define xmodmap(name) "/home/ch/.config/i3/xmodmap/" name
 
 /* commands */
+static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", NULL };
 static const char *termcmd[]  = { "st", NULL };
 static const char scratchpadname[] = "scratchpad";
@@ -131,10 +133,9 @@ static Key keys[] = {
 	{ MODKEY,                       XK_Tab,       view,           {0} },
 	{ MODKEY,                       XK_q,         killclient,     {0} },
 	{ MODKEY|ShiftMask,             XK_t,         setlayout,      {.v = &layouts[0]} },
-	{ MODKEY|ShiftMask,             XK_f,         setlayout,      {.v = &layouts[1]} },
 	{ MODKEY|ShiftMask,             XK_w,         setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       XK_f,         fullscreen,     {0} },
-	{ MODKEY,                       XK_space,     setlayout,      {0} },
+	{ MODKEY,                       XK_space,     setlayout,      {.v = &layouts[1]} },
 	{ MODKEY|ShiftMask,             XK_space,     togglefloating, {0} },
 	{ MODKEY,                       XK_0,         view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,         tag,            {.ui = ~0 } },
