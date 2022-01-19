37abb30c9418e43499dac920cd419563cb654791
rainbowhxch <rainbowhxch@gmail.com>
2022-01-19 14:51:03 +0800

    remove border and add icons to bar

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
+static const unsigned int borderpx  = 0;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayonleft = 0;   	/* 0: systray in the right corner, >0: systray on left of status text */
@@ -18,15 +18,13 @@ static const int topbar             = 1;        /* 0 means bottom bar */
 static const Bool viewontag         = True;     /* Switch view on tag switch */
 static const char *fonts[]          = { "JetBrainsMono Nerd Font:size=10" };
 static const char dmenufont[]       = "JetBrainsMono Nerd Font:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
+static const char col_fg[]          = "#c0caf5";
+static const char col_bg[]          = "#24283b";
+static const char col_border[]      = "#e0af68";
 static const char *colors[][3]      = {
-	/*               fg         bg         border   */
-	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	/*                fg       bg      border   */
+	[SchemeNorm] = { col_fg, col_bg, col_bg },
+	[SchemeSel]  = { col_fg, col_border, col_border },
 };
 
 /* tagging */
@@ -101,7 +99,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_a,         spawn,          {.v = gnomescreenshotcmd } },
 	{ MODKEY|ControlMask,           XK_a,         spawn,          {.v = arandrcmd } },
 	{ MODKEY|ControlMask,           XK_s,         spawn,          {.v = pavucontrolcmd } },
-	{ MODKEY|ControlMask,           XK_e,         spawn,          {.v = emojicmd } },
+	{ MODKEY,                       XK_apostrophe,spawn,          {.v = emojicmd } },
 	{ MODKEY,                       XK_n,         spawn,          {.v = neteasecmd } },
 	{ MODKEY|ShiftMask,             XK_n,         spawn,          {.v = clipmenucmd } },
 	{ MODKEY,                       XK_o,         spawn,          {.v = wpscmd } },
